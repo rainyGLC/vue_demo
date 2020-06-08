6eb1848e9db81e9b64be5a1c78cabd92c565aeda
rainyglc <2929939024@qq.com>
2020-06-08 18:04:41 +0800

    update

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -4,10 +4,32 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+
+
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+
+import ElementUI from 'element-ui'
+
+// import('element-ui/lib/theme-chalk/index.css')
+import 'element-ui/lib/theme-chalk/index.css';
+
+
+import './components/iconSvg' // iconSvg
+
+import '@/mockjs';  //mock数据 
+
+//i18N国际化
+import i18n from "@/lang";
+
+
 Vue.config.productionTip = false;
+Vue.use(ElementUI, { size: 'mini'});
+
 
 new Vue({
   router,
   store,
+  i18n, //便于可以直接在组件中通过this.$i18n使用，也可以按需引用
   render: h => h(App)
 }).$mount("#app");
--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,30 +1,43 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Router from "vue-router";
+import Home from "@/page/Home.vue";
 
-Vue.use(VueRouter);
+// Vue.use(Router);
+process.env.NODE_ENV === "development" ? Vue.use(Router) : null;
 
-const routes = [
+
+//默认不需要权限的页面
+
+export const constantRouterMap = [
   {
     path: "/",
     name: "Home",
     component: Home
   },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
+  { path: '/login',name: 'login',component:() => import('@/page/login'),hidden: true},
+]
 
-const router = new VueRouter({
+// const routes = [
+//   {
+//     path: "/",
+//     name: "Home",
+//     component: Home
+//   },
+//   {
+//     path: "/about",
+//     name: "About",
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () =>
+//       import(/* webpackChunkName: "about" */ "../views/About.vue")
+//   }
+// ];
+
+export default new Router({
   mode: "history",
-  base: process.env.BASE_URL,
-  routes
+  base: '/permission/', // 添加跟目录,对应服务器部署子目录
+  routes:constantRouterMap
 });
 
-export default router;
+// export default router;
